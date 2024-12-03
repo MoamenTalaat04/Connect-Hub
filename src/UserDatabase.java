a376078320451656e598a428ed719e207ad4c652
NadimHassan09 <es-nadim.hassan2027@alexu.edu.eg>
2024-12-03 20:10:10 +0200

    database

---
+++ src/UserDatabase.java
@@ -0,0 +1,19 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class UserDatabase {
+    private final String userDatabaseFile = "users.json";
+    private final ObjectMapper mapper ;
+    private ArrayList<User> users;
+    private Map accValidation;
+
+    public UserDatabase(String userDatabaseFile) {
+        this.userDatabaseFile = userDatabaseFile;
+        mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    }
+
+}
