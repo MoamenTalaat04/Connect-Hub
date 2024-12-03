2bd1908dc65f52bc6ab3763c50b5ba5728752085
Mohammed Adel <missmohamed6@gmail.com>
2024-12-03 21:11:21 +0200

    added the form for the NewsFeed

---
+++ src/NewsFeedWindow.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+public class NewsFeedWindow {
+    private JPanel panel1;
+    private JPanel NavigationPanal;
+    private JPanel FriendStatusPanal;
+    private JPanel NewsFeedPanal;
+    private JPanel PostsPanal;
+    private JPanel StoriesPanal;
+    private JScrollBar scrollBar1;
+    private JScrollBar scrollBar2;
+    private JButton profileButton;
+    private JButton friendsButton;
+    private JButton logOutButton;
+    private JButton refreshButton;
+    private JSeparator StoriesPostsSeparator;
+    private JSeparator NewsFeedNavigationSeparator;
+
+
+}
