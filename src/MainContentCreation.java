0356836d5fe460e2b12d6195366ce66cdef847f6
Mohammed Adel <missmohamed6@gmail.com>
2024-12-05 21:40:44 +0200

    Update MainContentCreation.java

--- src/MainContentCreation.java
+++ src/MainContentCreation.java
@@ -1,6 +1,7 @@
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import java.io.File;
 import java.io.IOException;
